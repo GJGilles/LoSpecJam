964d227178d2d3c3bcc15bf57bcef90b0214f46e
turtttle <gjgilles@uwaterloo.ca>
2021-07-10 23:59:21 -0400

    basic game outline

---
+++ Assets/Scripts/Controllers/FishController.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishController : MonoBehaviour
+{
+    public SpriteRenderer spr;
+    public float speed = 0.01f;
+    public int range = 50;
+
+    private float location = 0;
+    private int destination = 0;
+
+    void Update()
+    {
+        while(Mathf.RoundToInt(location) == destination)
+        {
+            destination = UnityEngine.Random.Range(-range, range);
+        }
+
+        float next = location + speed * Math.Sign(destination - Mathf.RoundToInt(location));
+        int diff = Mathf.RoundToInt(next) - Mathf.RoundToInt(location);
+        while (diff > 0)
+        {
+            spr.flipX = false;
+            transform.position = transform.position + new Vector3(1, 0);
+            diff--;
+        }
+        while (diff < 0)
+        {
+            spr.flipX = true;
+            transform.position = transform.position - new Vector3(1, 0);
+            diff++;
+        }
+
+        location = next;
+    }
+}
